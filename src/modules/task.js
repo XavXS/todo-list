81fb878666407f8b79323fa4306882c906fe0186
xav <xavs421@gmail.com>
2022-07-06 22:14:06 -0400

    Initialize task.js

---
+++ src/modules/task.js
@@ -0,0 +1,35 @@
+export default class task {
+    #content;
+
+    constructor(title, description, dueDate, priority) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.#generateContent();
+    }
+
+    getTitle() {
+        return this.title;
+    }
+
+    getDescription() {
+        return this.description;
+    }
+
+    getDueDate() {
+        return this.dueDate;
+    }
+
+    getPriority() {
+        return this.priority;
+    }
+
+    #generateContent() {
+        
+    }
+
+    getContent() {
+        return this.#content;
+    }
+};
